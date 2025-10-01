4f906f077c509800ed226b2077a03ed57a8ac7f7
Mustafa BİÇER <mstfbiccer@gmail.com>
2025-10-01 11:42:08 +0300

    add skills form component and user info table; integrate into app layout

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NameForm } from './components/hero-banner/common/user-form';
+import { SkillsFormComponent } from './components/hero-banner/common/dynamic-user-form';
+import { UserFormComponentWithSkills } from './components/hero-banner/common/dynamic-custom-form';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,NameForm],
+  imports: [RouterOutlet,NameForm, SkillsFormComponent, UserFormComponentWithSkills],
   templateUrl: './app.html',
   styleUrls: ['./app.scss'],
 })
